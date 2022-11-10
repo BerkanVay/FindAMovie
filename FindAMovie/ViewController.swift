00bb0d5362a086f8e22f19dc5f84a03cb5fd3a2f
MustafaBerkanVay <berkanvay@gmail.com>
2022-11-10 15:52:04 +0300

    Created the project

---
+++ FindAMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindAMovie
+//
+//  Created by Mustafa Berkan Vay on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
